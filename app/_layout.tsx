f345f8cc205bbeeae03a37e153a9c93b70129f07
Alexs Ruiz <ralexs.acu@gmail.com>
2024-02-10 03:24:32 -0600

    Add the main Layout

---
+++ app/_layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Stack } from 'expo-router'
+
+const Layout = () => {
+  return (
+    <Stack screenOptions={{
+      headerStyle: {
+        backgroundColor: '#f4511e'
+      },
+      headerTintColor: '#fff'
+    }}>
+      <Stack.Screen name='index' options={{ title: 'Pokedex' }} />
+      <Stack.Screen name='(pokemon)/[id]' options={{ title: '' }} />
+    </Stack>
+  )
+}
+
+export default Layout
