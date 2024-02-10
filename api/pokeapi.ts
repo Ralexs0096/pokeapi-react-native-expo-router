759028070d33095d4769dd53f8fac7c37f885a0f
Alexs Ruiz <ralexs.acu@gmail.com>
2024-02-10 03:23:36 -0600

    Create the API methods

---
+++ api/pokeapi.ts
@@ -0,0 +1,31 @@
+export interface Pokemon {
+  name: string;
+  url: string;
+  id: number;
+  image: string;
+  sprites?: any;
+  abilities?: any;
+  stats?: any;
+}
+
+export const getPokemon = async (limit = 150) => {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}`
+  );
+
+  const data = await response.json();
+  return data.results.map((pokemon: Pokemon, index: number) => ({
+    ...pokemon,
+    id: index + 1,
+    image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+      index + 1
+    }.png`
+  }));
+};
+
+export const getPokemonDetails = async (id: number) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+
+  const data = await response.json();
+  return data;
+};
