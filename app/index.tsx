c85c76447070249c1c09cf0a1c49e9eb3fc1855d
Alexs Ruiz <ralexs.acu@gmail.com>
2024-02-11 23:45:48 -0600

    Remove unnecessary imports

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { ScrollView, Text, View, StyleSheet, TouchableOpacity, Image, ActivityIndicator } from 'react-native';
 import { Link } from 'expo-router';
 import { Pokemon, getPokemon } from '@/api/pokeapi';
