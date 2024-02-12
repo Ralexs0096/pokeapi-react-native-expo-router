d076299b416fe752918238a743f1b37c62953141
Alexs Ruiz <ralexs.acu@gmail.com>
2024-02-12 01:00:44 -0600

    Add new Page: Favorites
    - provide some format to modified fields

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,43 @@
-import React from 'react'
-import { Stack } from 'expo-router'
+import React from 'react';
+import { Stack, Link } from 'expo-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 const queryClient = new QueryClient();
 
 const Layout = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Stack screenOptions={{
-        headerStyle: {
-          backgroundColor: '#f4511e'
-        },
-        headerTintColor: '#fff'
-      }}>
-        <Stack.Screen name='index' options={{ title: 'Pokedex' }} />
-        <Stack.Screen name='(pokemon)/[id]' options={{ title: '' }} />
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#f4511e'
+          },
+          headerTintColor: '#fff'
+        }}
+      >
+        <Stack.Screen
+          name="index"
+          options={{
+            title: 'Pokedex',
+            headerRight: () => (
+              <Link href={'/favorites'} asChild>
+                <TouchableOpacity>
+                  <Ionicons name="heart-circle" size={26} color={'#fff'} />
+                </TouchableOpacity>
+              </Link>
+            )
+          }}
+        />
+        <Stack.Screen name="(pokemon)/[id]" options={{ title: '' }} />
+        <Stack.Screen
+          name="favorites"
+          options={{ title: 'Favorites', presentation: 'modal' }}
+        />
       </Stack>
     </QueryClientProvider>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
