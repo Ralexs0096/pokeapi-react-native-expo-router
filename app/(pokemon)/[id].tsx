21dec7dd2a4eb85d2f05ab5df61528041b33b956
Alexs Ruiz <ralexs.acu@gmail.com>
2024-02-12 01:01:44 -0600

    Provide format to existing files

--- app/(pokemon)/[id].tsx
+++ app/(pokemon)/[id].tsx
@@ -1,81 +1,93 @@
-import { View, Text, StyleSheet, Image } from 'react-native'
-import React, { useEffect } from 'react'
-import { useLocalSearchParams, useNavigation } from 'expo-router'
-import { Pokemon, getPokemonDetails } from '@/api/pokeapi'
+import { View, Text, StyleSheet, Image } from 'react-native';
+import React, { useEffect } from 'react';
+import { useLocalSearchParams, useNavigation } from 'expo-router';
+import { Pokemon, getPokemonDetails } from '@/api/pokeapi';
 import { Ionicons } from '@expo/vector-icons';
 import { useQuery, keepPreviousData } from '@tanstack/react-query';
 import { storage } from '@/storage/mmkv';
 import { useMMKVBoolean } from 'react-native-mmkv';
-import Animated, { FadeIn, FadeInDown, FlipInEasyX } from 'react-native-reanimated';
+import Animated, {
+  FadeIn,
+  FadeInDown,
+  FlipInEasyX
+} from 'react-native-reanimated';
 
 const Page = () => {
-  const { id } = useLocalSearchParams<{ id: string }>()
-  const [isFavorite, setIsFavorite] = useMMKVBoolean(`favorite-${id}`, storage)
-  const navigation = useNavigation()
+  const { id } = useLocalSearchParams<{ id: string }>();
+  const [isFavorite, setIsFavorite] = useMMKVBoolean(`favorite-${id}`, storage);
+  const navigation = useNavigation();
 
   const { data } = useQuery<Pokemon>({
     queryKey: ['pokemon', id],
     queryFn: () => getPokemonDetails(+id!),
-    // keepPreviousData: removed in favor of `placeholderData` identity function, 
+    // keepPreviousData: removed in favor of `placeholderData` identity function,
     // reference: https://tanstack.com/query/v5/docs/framework/react/guides/migrating-to-v5#removed-keeppreviousdata-in-favor-of-placeholderdata-identity-function
     placeholderData: keepPreviousData,
     // onSuccess: is not longer supported, so we will depend of a useEffect below
     // reference: https://tanstack.com/query/latest/docs/framework/react/guides/migrating-to-react-query-4#onsuccess-is-no-longer-called-from-setquerydata
     refetchOnMount: false
-  })
+  });
 
   // onSuccess - latest tankStack query implementation
   useEffect(() => {
     if (data) {
       navigation.setOptions({
         title: data.name.charAt(0).toUpperCase() + data.name.slice(1)
-      })
+      });
     }
-  }, [data])
-
+  }, [data]);
 
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => (
         <Text onPress={toggleFavorite}>
-          <Ionicons name={isFavorite ? 'star' : 'star-outline'} size={22} color="#fff" />
+          <Ionicons
+            name={isFavorite ? 'star' : 'star-outline'}
+            size={22}
+            color="#fff"
+          />
         </Text>
       )
-    })
-  }, [isFavorite])
+    });
+  }, [isFavorite]);
 
   const toggleFavorite = async () => {
-    setIsFavorite(isFavorite ? false : true)
-  }
+    setIsFavorite(isFavorite ? false : true);
+  };
 
   return (
     <View style={{ padding: 10 }}>
-      {
-        data && (
-          <>
-            {isFavorite}
-            <Animated.View style={[styles.card, { alignItems: 'center' }]} entering={FadeIn.delay(200)}>
-              <Image source={{ uri: data.sprites.front_default }} style={{ width: 200, height: 200 }} />
-              <Animated.Text style={styles.name} entering={FlipInEasyX.delay(300)}>
-                #{data.id} {data.name}
-              </Animated.Text>
-            </Animated.View>
-            <Animated.View style={styles.card} entering={FadeInDown.delay(500)}>
-              <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Stats: </Text>
-              {
-                data.stats.map((item: any) => (
-                  <Text key={item.stat.name}>
-                    {item.stat.name}: {item.base_stat}
-                  </Text>
-                ))
-              }
-            </Animated.View>
-          </>
-        )
-      }
+      {data && (
+        <>
+          {isFavorite}
+          <Animated.View
+            style={[styles.card, { alignItems: 'center' }]}
+            entering={FadeIn.delay(200)}
+          >
+            <Image
+              source={{ uri: data.sprites.front_default }}
+              style={{ width: 200, height: 200 }}
+            />
+            <Animated.Text
+              style={styles.name}
+              entering={FlipInEasyX.delay(300)}
+            >
+              #{data.id} {data.name}
+            </Animated.Text>
+          </Animated.View>
+          <Animated.View style={styles.card} entering={FadeInDown.delay(500)}>
+            <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Stats: </Text>
+            {data.stats.map((item: any) => (
+              <Text key={item.stat.name}>
+                {item.stat.name}: {item.base_stat}
+              </Text>
+            ))}
+          </Animated.View>
+        </>
+      )}
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   card: {
@@ -97,6 +109,6 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textTransform: 'capitalize'
   }
-})
+});
 
-export default Page
+export default Page;
--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, Image, ActivityIndicator } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  ActivityIndicator
+} from 'react-native';
 import { Link } from 'expo-router';
 import { Pokemon, getPokemon } from '@/api/pokeapi';
 import { Ionicons } from '@expo/vector-icons';
 import { useQuery } from '@tanstack/react-query';
 import { FlashList, ListRenderItem } from '@shopify/flash-list';
 
-
 const Page = () => {
   const pokemonQuery = useQuery<Pokemon[]>({
     queryKey: ['pokemon'],
     queryFn: () => getPokemon(),
     refetchOnMount: false
-  })
+  });
 
   const renderItem: ListRenderItem<Pokemon> = ({ item }) => (
     <Link href={`/(pokemon)/${item.id}`} key={item.id} asChild>
@@ -20,11 +26,11 @@ const Page = () => {
         <View style={styles.item}>
           <Image source={{ uri: item.image }} style={styles.preview} />
           <Text style={styles.itemText}>{item.name}</Text>
-          <Ionicons name='chevron-forward' size={24} />
+          <Ionicons name="chevron-forward" size={24} />
         </View>
       </TouchableOpacity>
     </Link>
-  )
+  );
 
   return (
     <View style={{ flex: 1 }}>
@@ -34,7 +40,11 @@ const Page = () => {
         data={pokemonQuery.data}
         renderItem={renderItem}
         estimatedItemSize={100}
-        ItemSeparatorComponent={() => <View style={{ height: 1, width: '100%', backgroundColor: '#dfdfdf' }} />}
+        ItemSeparatorComponent={() => (
+          <View
+            style={{ height: 1, width: '100%', backgroundColor: '#dfdfdf' }}
+          />
+        )}
       />
     </View>
   );
@@ -57,6 +67,6 @@ const styles = StyleSheet.create({
     textTransform: 'capitalize',
     flex: 1
   }
-})
+});
 
 export default Page;
