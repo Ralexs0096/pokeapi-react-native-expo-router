4abf717bade5feff24df8a7bd797863714492f7a
Alexs Ruiz <ralexs.acu@gmail.com>
2024-02-15 13:39:22 -0600

    Replicate the Pokemon Card
    - add basic entering animation
    - add functionality to remove the selected pokemon

--- app/favorites.tsx
+++ app/favorites.tsx
@@ -1,8 +1,16 @@
-import { Text, ScrollView } from 'react-native';
-import React, { useState } from 'react';
+import {
+  Text,
+  ScrollView,
+  Image,
+  StyleSheet,
+  TouchableOpacity
+} from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { useQueries } from '@tanstack/react-query';
 import { storage } from '@/storage/mmkv';
 import { getPokemonDetails, Pokemon } from '@/api/pokeapi';
+import { Ionicons } from '@expo/vector-icons';
+import Animated, { FadeIn, SlideOutLeft } from 'react-native-reanimated';
 
 const Page = () => {
   const [keys, setKeys] = useState(storage.getAllKeys());
@@ -18,15 +26,60 @@ const Page = () => {
     })
   });
 
+  const allFinished = pokemonQueries.every((query) => query.isSuccess);
+
+  useEffect(() => {
+    if (allFinished) {
+      setData(pokemonQueries.map((query) => query.data));
+    }
+  }, [allFinished]);
+
+  const removeFavorite = (id: number) => {
+    storage.delete(`favorite-${id}`);
+    setData((prev) => {
+      return prev.filter((pokemon) => pokemon.id !== id);
+    });
+  };
+
   return (
     <ScrollView>
-      {pokemonQueries.map((query) => (
-        <Text key={`${query?.data?.id}-${query?.data?.name}`}>
-          {query?.data?.name ?? 'test'}
-        </Text>
+      {data.map(({ name, id, sprites }, index) => (
+        <Animated.View
+          entering={FadeIn.delay(100 * index)}
+          exiting={SlideOutLeft.duration(200)}
+          style={styles.item}
+        >
+          <Image
+            source={{ uri: sprites.front_default }}
+            style={styles.preview}
+          />
+          <Text style={styles.itemText}>{name}</Text>
+          <TouchableOpacity onPress={() => removeFavorite(id)}>
+            <Ionicons name="trash" size={24} color="red" />
+          </TouchableOpacity>
+        </Animated.View>
       ))}
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 10,
+    height: 100,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  preview: {
+    width: 100,
+    height: 100
+  },
+  itemText: {
+    fontSize: 18,
+    textTransform: 'capitalize',
+    flex: 1
+  }
+});
 
 export default Page;
