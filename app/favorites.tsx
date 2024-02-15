a6349a8fc50e5af6c0e13d2ea37fcf9fa08aad78
Alexs Ruiz <ralexs.acu@gmail.com>
2024-02-15 11:42:45 -0600

    Get all favorites pokemon

--- app/favorites.tsx
+++ app/favorites.tsx
@@ -1,11 +1,31 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { Text, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { useQueries } from '@tanstack/react-query';
+import { storage } from '@/storage/mmkv';
+import { getPokemonDetails, Pokemon } from '@/api/pokeapi';
 
 const Page = () => {
+  const [keys, setKeys] = useState(storage.getAllKeys());
+  const [data, setData] = useState<Pokemon[]>([]);
+
+  const pokemonQueries = useQueries({
+    queries: keys.map((key) => {
+      const pokemonId = key.split('-')[1];
+      return {
+        queryKey: ['pokemon', pokemonId],
+        queryFn: () => getPokemonDetails(+pokemonId)
+      };
+    })
+  });
+
   return (
-    <View>
-      <Text>Favorites</Text>
-    </View>
+    <ScrollView>
+      {pokemonQueries.map((query) => (
+        <Text key={`${query?.data?.id}-${query?.data?.name}`}>
+          {query?.data?.name ?? 'test'}
+        </Text>
+      ))}
+    </ScrollView>
   );
 };
 
